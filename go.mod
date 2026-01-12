a1330688dbad283cd2238c83366cccefa3c20612
Achmad Alfan Ahsani <achmadalfanahsani@gmail.com>
2026-01-12 15:02:06 +0700

    update method SayHello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/achmadalfanahsani/pzn-go-say-hello
+module github.com/achmadalfanahsani/pzn-go-say-hello/v2
 
 go 1.24.1
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package pzn_go_say_hello
 
-func SayHello() string {
-	return "Hello, World!"
+func SayHello(name string) string {
+	return "Hello " + name
 }
